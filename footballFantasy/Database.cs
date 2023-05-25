3095aa850caaac48be3e0fbfc6e61c890437bd95
SEPEHR <sphrm26sphrm26@gmail.com>
2023-05-25 06:30:24 +0430

    add user table to database and set userName as primary key

--- footballFantasy/Database.cs
+++ footballFantasy/Database.cs
@@ -1,17 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
 namespace footballFantasy
 {
     public class Database : DbContext
     {
+        public DbSet<User> users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder contextOptionsBuilder)
         {
             contextOptionsBuilder.UseSqlite("Data source=database.db");
         }
     }
 
-    class User
+    public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string userName { get; set; }
         public string name { get; set; }
         public string email { get; set; }

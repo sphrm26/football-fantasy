1a49a918db591dd9c544278e6d22847f8a378df7
SEPEHR <sphrm26sphrm26@gmail.com>
2023-06-01 13:30:00 +0430

    layering Project is starting

---
+++ footballFantasy/Model/waitingUsers.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace footballFantasy.Model
+{
+    public class waitingUsers
+    {
+        [Key]
+        public string userName { get; set; }
+        public DateTime dt { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string OTP { get; set; }
+
+        public waitingUsers(string Password, DateTime dt, string name, string email, string username, string OTP)
+        {
+            this.OTP = OTP;
+            this.dt = dt;
+            this.name = name;
+            this.email = email;
+            userName = username;
+            password = password;
+        }
+        public bool checkExpire()
+        {
+            TimeSpan timesAfterSending = DateTime.Now - dt;
+            if (timesAfterSending.TotalMinutes > 5)
+            {
+                return true;
+            }
+            return false;
+        }
+        public static void checkAllOTPForExpire()
+        {
+            using (var db = new Database())
+            {
+                foreach (var item in db.waitingListUsers)
+                {
+                    if (item.checkExpire())
+                    {
+                        db.waitingListUsers.Remove(item);
+                    }
+                }
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ footballFantasy/PresentationLayer/OTPChecking.cs
@@ -0,0 +1,32 @@
+﻿using footballFantasy.Model;
+
+namespace footballFantasy.PresentationLayer
+{
+    public class OTPChecking
+    {
+        public static string OTPCheck(string otp, string email)
+        {
+            using (var db = new Database())
+            {
+                foreach (var item in db.waitingListUsers)
+                {
+                    if (item.email == email && item.OTP == otp)
+                    {
+                        if (item.checkExpire())
+                        {
+                            waitingUsers.checkAllOTPForExpire();
+                            return "your OTP is expired";
+                        }
+                        db.users.Add(new User(item.password, item.name, item.email, item.userName));
+                        db.waitingListUsers.Remove(item);
+                        db.SaveChanges();
+                        waitingUsers.checkAllOTPForExpire();
+                        return "your OTP is correct";
+                    }
+                }
+            }
+            waitingUsers.checkAllOTPForExpire();
+            return "your OTP is incorrect";
+        }
+    }
+}

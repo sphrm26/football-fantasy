b4c2852c60287f6e0829d9afb3db3fdd6b1ff13b
SEPEHR <sphrm26sphrm26@gmail.com>
2023-06-08 11:48:52 +0330

    email validation completed and regex of email edited

--- TestProject/emailTest.cs
+++ TestProject/emailTest.cs
@@ -6,6 +6,35 @@ public class emailTest
     [TestMethod]
     public void atsignCheck()
     {
-        footballFantasy.BuisnessLayer.validaitonSignUp.validationName("sDg4p@hrm@gmail.com");
+        footballFantasy.BuisnessLayer.validaitonSignUp.validationEmail("sgv@hrm@gmail.com");
+    }
+
+    [TestMethod]
+    public void noneAtsign()
+    {
+        footballFantasy.BuisnessLayer.validaitonSignUp.validationEmail("sgvhrmgmail.com");
+    }
+
+    [TestMethod]
+    public void dotCheck()
+    {
+        footballFantasy.BuisnessLayer.validaitonSignUp.validationEmail("f.ghb@gmail.com");
+    }
+
+    [TestMethod]
+    public void startWithDot()
+    {
+        footballFantasy.BuisnessLayer.validaitonSignUp.validationEmail(".ghb@gmail.com");
+    }
+
+    [TestMethod]
+    public void endWithDot()
+    {
+        footballFantasy.BuisnessLayer.validaitonSignUp.validationEmail("ghb.@gmail.com");
+    }
+    [TestMethod]
+    public void domainCheck()
+    {
+        footballFantasy.BuisnessLayer.validaitonSignUp.validationEmail("ghb@gmail.co.m");
     }
 }

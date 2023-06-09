548f035cb3f5a5bfd15e36127d05b90ea0b620d5
amirali-ramandi <amir.kava.kava@gmail.com>
2023-06-09 11:56:57 +0330

    add api request

---
+++ footballFantasy/BuisnessLayer/apiRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace footballFantasy.BuisnessLayer
+{
+    public class Class
+    {
+        string url = "https://fantasy.premierleague.com/api/bootstrap-static/";
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+        HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        Stream resStream = response.GetResponseStream();
+        StreamReader reader = new StreamReader(resStream);
+        string str = reader.ReadToEnd();
+    }
+}

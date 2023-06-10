f0ed21c3c049d81a6537bbcd31e82d8e11a9f2b5
SEPEHR <sphrm26sphrm26@gmail.com>
2023-06-10 15:56:09 +0330

    OTPCheck api now just call buisnes layer

--- footballFantasy/BuisnessLayer/UserHandel.cs
+++ footballFantasy/BuisnessLayer/UserHandel.cs
@@ -41,5 +41,26 @@ namespace footballFantasy.BuisnessLayer
             waitingUsers newWaitUser = new waitingUsers(password, DateTime.Now, name, email, username, code);
             handelUserDatabase.addToWaitList(newWaitUser);
         }
+        public static string OTPCheck(string otp, string email)
+        {
+
+            var waitUser = handelUserDatabase.findWaitUser(otp, email);
+            if (waitUser == null)
+            {
+                handelExpireOTPDatabase.checkAllOTPForExpire();
+                return "your OTP is incorrect";
+            }
+            if (expireOTP.checkExpire(waitUser))
+            {
+                handelExpireOTPDatabase.checkAllOTPForExpire();
+                return "your OTP is expired";
+            }
+            //add to user class
+            User newUser = new User(waitUser.password, waitUser.name, waitUser.email, waitUser.userName);
+            handelUserDatabase.addToUsers(newUser);
+            //remove from wait list
+            handelUserDatabase.removeFromWaitList(email);
+            return "your OTP is correct\nsuccessfuly sign up";
+        }
     }
 }
--- footballFantasy/PresentationLayer/OTPChecking.cs
+++ footballFantasy/PresentationLayer/OTPChecking.cs
@@ -8,23 +8,7 @@ namespace footballFantasy.PresentationLayer
     {
         public static string OTPCheck(string otp, string email)
         {
-            var waitUser = handelUserDatabase.findWaitUser(otp, email);
-            if (waitUser == null)
-            {
-                handelExpireOTPDatabase.checkAllOTPForExpire();
-                return "your OTP is incorrect";
-            }
-            if (expireOTP.checkExpire(waitUser))
-            {
-                handelExpireOTPDatabase.checkAllOTPForExpire();
-                return "your OTP is expired";
-            }
-            //add to user class
-            User newUser = new User(waitUser.password, waitUser.name, waitUser.email, waitUser.userName);
-            handelUserDatabase.addToUsers(newUser);
-            //remove from wait list
-            handelUserDatabase.removeFromWaitList(email);
-            return "your OTP is correct\nsuccessfuly sign up";
+            return UserHandel.OTPCheck(otp, email);
         }
     }
 }

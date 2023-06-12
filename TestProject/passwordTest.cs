7b52db897d0384fd2dd512d7a3a27337412a2315
SEPEHR <sphrm26sphrm26@gmail.com>
2023-06-12 18:45:32 +0330

    write test case for password validation

--- TestProject/passwordTest.cs
+++ TestProject/passwordTest.cs
@@ -20,6 +20,21 @@ public class passwordTest
         footballFantasy.BuisnessLayer.validaitonSignUp.passwordCorrectionCheck("DKC5FKk");
     }
     [TestMethod]
+    public void uncorrectSymboleChck()
+    {
+        footballFantasy.BuisnessLayer.validaitonSignUp.passwordCorrectionCheck("dkf☺DXK32");
+    }
+    [TestMethod]
+    public void spaceCheck()
+    {
+        footballFantasy.BuisnessLayer.validaitonSignUp.passwordCorrectionCheck("dkfxD XK32");
+    }
+    [TestMethod]
+    public void symboleCheck()
+    {
+        footballFantasy.BuisnessLayer.validaitonSignUp.passwordCorrectionCheck("dD3@#$%^&*+=_!");
+    }
+    [TestMethod]
     public void correctForm()
     {
         footballFantasy.BuisnessLayer.validaitonSignUp.passwordCorrectionCheck("dkfxDXK32");

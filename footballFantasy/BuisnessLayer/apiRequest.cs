6a41955a76bf5f0db60af9822ad3107f80a19bcd
amirali-ramandi <amir.kava.kava@gmail.com>
2023-06-14 15:57:45 +0330

    deserialize

--- footballFantasy/BuisnessLayer/apiRequest.cs
+++ footballFantasy/BuisnessLayer/apiRequest.cs
@@ -6,13 +6,13 @@ namespace footballFantasy.BuisnessLayer
 {
     public class Response
     {
-        public List<Player> elements { get; set; }
+        public List<Player> player_seter { get; set; }
     }
     public class apiRequest
     {
       public void  json2csharp()
         {
-            int num = 0;
+ 
             string url = "https://fantasy.premierleague.com/api/bootstrap-static/";
             var response = url.GetJsonFromUrl().FromJson<Response>();
         }

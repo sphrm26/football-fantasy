e9b87baa1a3e5bcb47ec4141be93d82b0439db8f
CePrhassani <programhasani@gmail.com>
2023-06-15 21:54:05 +0330

    saving player to database completed

---
+++ footballFantasy/DataAccessLayer/PlayerHandle.cs
@@ -0,0 +1,23 @@
+﻿using footballFantasy.Model;
+
+namespace footballFantasy.DataAccessLayer;
+
+public class PlayerHandle
+{
+    public static void SavingPlayrToDb(List<Player> newPlayers)
+    {
+        using (var db = new Database())
+        {
+            foreach (var player  in db.Players)
+            {
+                db.Players.Remove(player);
+            }
+            
+            foreach (var player in newPlayers)
+            {
+                db.Players.Add(player);
+            }
+            db.SaveChanges();
+        } 
+    } 
+}

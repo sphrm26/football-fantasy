dc9a5e3a0f5778f4b595ba36b4efbdcb7e663864
SEPEHR <sphrm26sphrm26@gmail.com>
2023-07-06 12:23:32 +0330

    completed sub players and saving changes of team in database

--- footballFantasy/BuisnessLayer/substitut.cs
+++ footballFantasy/BuisnessLayer/substitut.cs
@@ -1,6 +1,13 @@
-﻿namespace footballFantasy.BuisnessLayer
+﻿using footballFantasy.Model;
+
+namespace footballFantasy.BuisnessLayer
 {
     public class substitut
     {
+        public static void sub(User user,int code1,int code2)
+        {
+            //swapp
+            DataAccessLayer.handelUserDatabase.saveChanges(user);
+        }
     }
 }

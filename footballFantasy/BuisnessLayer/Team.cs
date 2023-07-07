5a763268455c0217db271e14bf19797f696c0661
Mohammad Saleh <sadqynzhadm9@gmail.com>
2023-07-07 10:21:35 +0430

    write teammatesNumCheck in addplayer

--- footballFantasy/BuisnessLayer/Team.cs
+++ footballFantasy/BuisnessLayer/Team.cs
@@ -1,4 +1,5 @@
-﻿using footballFantasy.Model;
+﻿using System.Linq.Expressions;
+using footballFantasy.Model;
 using footballFantasy.DataAccessLayer;
 namespace footballFantasy.BuisnessLayer;
 
@@ -12,25 +13,71 @@ public class Team
     public List<Player> insideDEF { get; set; }
     public List<Player> insideMID { get; set; }
     public List<Player> insideFRW { get; set; }
-    public static void addplayer(string userName, int code)
-    {
-        var playerWantToAdd = handlePlayer.findPlayerByCode(code);
-        var userWantToAdd = handelUserDatabase.findUserByUserName(userName);
-    }
-    public static void positionLimit()
+    public void addplayer(Player player)
     {
         
     }
-    public static void positionCheck()
+    public void positionLimit()
     {
         
     }
-    public static void budgetCheck()
+    public void positionCheck()
+    {
+        
+    }
+    public void budgetCheck()
     {
 
     }
-    public static void teammatesNumCheck()
+    public void teammatesNumCheck(Player newPlayer)
     {
-        
+        int sameTeamNum = 0;
+        if (outsideGK.team == newPlayer.team )
+        {
+            sameTeamNum++;
+        }
+        if (outsideDEF.team == newPlayer.team)
+        {
+            sameTeamNum++;
+        }
+        if (outsideMID.team == newPlayer.team)
+        {
+            sameTeamNum++;
+        }
+        if (outsideFRW.team == newPlayer.team)
+        {
+            sameTeamNum++;
+        }
+        if (insideGK.team == newPlayer.team)
+        {
+            sameTeamNum++;
+        }
+
+        foreach (var player in insideDEF)
+        {
+            if (player.team == newPlayer.team)
+            {
+                sameTeamNum++;
+            }
+        }
+        foreach (var player in insideMID)
+        {
+            if (player.team == newPlayer.team)
+            {
+                sameTeamNum++;
+            }
+        }
+        foreach (var player in insideFRW)
+        {
+            if (player.team == newPlayer.team)
+            {
+                sameTeamNum++;
+            }
+        }
+
+        if (sameTeamNum > 3)
+        {
+            throw new Exception("you cant add more than 3 player from same team");
+        }
     }
 }

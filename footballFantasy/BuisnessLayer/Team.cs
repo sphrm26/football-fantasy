1ab7e395ee3d62167edabe4f6d0d3722da520d63
Mohammad Saleh <sadqynzhadm9@gmail.com>
2023-07-07 09:38:31 +0430

    add some methods to add player

--- footballFantasy/BuisnessLayer/Team.cs
+++ footballFantasy/BuisnessLayer/Team.cs
@@ -1,16 +1,36 @@
 ﻿using footballFantasy.Model;
-
+using footballFantasy.DataAccessLayer;
 namespace footballFantasy.BuisnessLayer;
 
 public class Team
 {
-    public Player outsideGK { get; set; }
+    public Player outsideGK { get; set; } 
     public Player outsideDEF { get; set; }
     public Player outsideMID { get; set; }
     public Player outsideFRW { get; set; }
     public Player insideGK { get; set; }
     public List<Player> insideDEF { get; set; }
-    public  List<Player> insideMID { get; set; }
-    public  List<Player> insideFRW { get; set; }
-    
+    public List<Player> insideMID { get; set; }
+    public List<Player> insideFRW { get; set; }
+    public static void addplayer(string userName, int code)
+    {
+        var playerWantToAdd = handlePlayer.findPlayerByCode(code);
+        var userWantToAdd = handelUserDatabase.findUserByUserName(userName);
+    }
+    public static void positionLimit()
+    {
+        
+    }
+    public static void positionCheck()
+    {
+        
+    }
+    public static void budgetCheck()
+    {
+
+    }
+    public static void teammatesNumCheck()
+    {
+        
+    }
 }
---
+++ footballFantasy/DataAccessLayer/handlePlayer.cs
@@ -0,0 +1,15 @@
+﻿using footballFantasy.Model;
+
+namespace footballFantasy.DataAccessLayer;
+
+public class handlePlayer
+{
+    public static Player? findPlayerByCode(int code)
+    {   
+        using (var db = new Database())
+        {
+            var record = db.Players.FirstOrDefault(record => record.code == code);
+            return record;
+        }
+    }
+}

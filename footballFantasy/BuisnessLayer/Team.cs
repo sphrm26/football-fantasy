96484df0a0e9ff6f3b117ed5b829f1ef2dc6b5b7
CePrhassani <programhasani@gmail.com>
2023-07-07 12:06:22 +0330

    adding method for delete player

--- footballFantasy/BuisnessLayer/Team.cs
+++ footballFantasy/BuisnessLayer/Team.cs
@@ -35,5 +35,51 @@ public class Team
         return list;
     }
 
+    public void deletePlayer(Player player)
+    {
+        if (insideGK.code == player.code)
+        {
+            insideGK = null ;
+        } 
+        
+        if (outsideGK.code == player.code)
+        {
+            outsideGK = null ;
+        } 
+        if (outsideDEF.code == player.code)
+        {
+            outsideDEF = null ;
+        } 
+        if (outsideMID.code == player.code)
+        {
+            outsideMID = null ;
+        } 
+        if (outsideFRW.code == player.code)
+        {
+            outsideFRW = null ;
+        }
+
+        foreach (var item in insideDEF)
+        {
+            if (item.code == player.code)
+            {
+                insideDEF.Remove(item);
+            } 
+        }
+        foreach (var item in insideMID)
+        {
+            if (item.code == player.code)
+            {
+                insideMID.Remove(item);
+            } 
+        }
+        foreach (var item in insideFRW)
+        {
+            if (item.code == player.code)
+            {
+                insideFRW.Remove(item);
+            } 
+        }
+    }
 
 }

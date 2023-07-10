ceb02011354f90b2fe3de21b348f22c171fb4fde
CePrhassani <programhasani@gmail.com>
2023-07-10 17:42:41 +0330

    makin api for getting user money

---
+++ footballFantasy/PresentationLayer/getMoney.cs
@@ -0,0 +1,12 @@
+﻿using footballFantasy.Model;
+
+namespace footballFantasy.PresentationLayer;
+
+public class getMoney
+{
+    public static string gettingMoney(string token)
+    {
+       User user =BuisnessLayer.UserHandel.GetUserByToken(token);
+       return Convert.ToString(user.budget);
+    }
+}

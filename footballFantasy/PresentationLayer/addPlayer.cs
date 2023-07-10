2059a4a3bb775b702015acba80715afddad661f0
amirali-ramandi <amir.kava.kava@gmail.com>
2023-07-10 21:15:31 +0330

    amir-nimekare addplayer

---
+++ footballFantasy/PresentationLayer/addPlayer.cs
@@ -0,0 +1,22 @@
+﻿using footballFantasy.Model;
+
+namespace footballFantasy.PresentationLayer
+{
+    public class addPlayer
+    {
+         public static string add_Player(string tk,int code)
+        {
+
+            User 
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                return ex.Message;
+            }
+            return "successfully added";
+        }
+    }
+}

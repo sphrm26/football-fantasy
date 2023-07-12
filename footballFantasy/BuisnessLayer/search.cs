377ea3b464a17a7ecf9d083784d86dc81a43dbaa
SEPEHR <sphrm26sphrm26@gmail.com>
2023-07-12 08:04:34 +0330

    completing search method

--- footballFantasy/BuisnessLayer/search.cs
+++ footballFantasy/BuisnessLayer/search.cs
@@ -1,6 +1,20 @@
-﻿namespace footballFantasy.BuisnessLayer
+﻿using footballFantasy.Model;
+
+namespace footballFantasy.BuisnessLayer
 {
     public class search
     {
+        public static List<Player> searchPlayers()
+        {
+            List<Player> list = new List<Player>();
+            List<Player> players = DataAccessLayer.PlayerHandle.GetAllPlayers();
+            foreach (var player in players)
+            {
+                // checking filters
+                // call search engine
+                // add to list
+            }
+            return list;
+        }
     }
 }

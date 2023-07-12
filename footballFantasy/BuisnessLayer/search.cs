0a10bb23610219210dc8cbad16827b4d4acdfc1d
SEPEHR <sphrm26sphrm26@gmail.com>
2023-07-12 08:08:34 +0330

    addin filter method

--- footballFantasy/BuisnessLayer/search.cs
+++ footballFantasy/BuisnessLayer/search.cs
@@ -4,7 +4,14 @@ namespace footballFantasy.BuisnessLayer
 {
     public class search
     {
-        public static List<Player> searchPlayers()
+        public static bool filterCheking(int minPrice, int maxPrice, int Position, int minScore, int maxScore,int teamCode)
+        {
+            //check in price range
+            //check position
+            //check in range score
+            //check team
+        }
+        public static List<Player> searchPlayers(string name, int minPrice, int maxPrice, int Position, int minScore, int maxScore, int teamCode)
         {
             List<Player> list = new List<Player>();
             List<Player> players = DataAccessLayer.PlayerHandle.GetAllPlayers();
